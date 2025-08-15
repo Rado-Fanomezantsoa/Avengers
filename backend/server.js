292977568d6998810d47698207d19143df9b5477
Rado <hei.fanomezantsoa@gmail.com>
2025-08-15 10:16:57 +0300

    feat: ajout du listen et les endpointes put et post

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,9 @@ const readCharacters = () => {
   return JSON.parse(data);
 };
 
-
+const writeCharacters = (characters) => {
+  fs.writeFileSync(DATA_FILE, JSON.stringify(characters, null, 2));
+};
 
 
 
@@ -32,3 +34,45 @@ app.get("/characters/:id", (req, res) => {
     return res.status(404).json({ message: "Character not found" });
   res.json(character);
 });
+
+
+app.post("/characters", (req, res) => {
+  const { name, realName, universe } = req.body;
+  if (!name || !realName || !universe) {
+    return res
+      .status(400)
+      .json({ message: "name, realName and universe are required" });
+  }
+  const characters = readCharacters();
+  const newCharacter = { id: Date.now(), name, realName, universe };
+  characters.push(newCharacter);
+  writeCharacters(characters);
+  res.status(201).json(newCharacter);
+});
+
+
+app.put("/characters/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const { name, realName, universe } = req.body;
+  let characters = readCharacters();
+  const index = characters.findIndex((c) => c.id === id);
+  
+  if (index === -1)
+    return res.status(404).json({ message: "Character not found" });
+  
+  characters[index] = {
+    ...characters[index],
+    ...(name && { name }),
+    ...(realName && { realName }),
+    ...(universe && { universe }),
+  };
+  writeCharacters(characters);
+  res.json(characters[index]);
+});
+
+
+
+
+app.listen(port, () => {
+  console.log(`Le serveur est en marche sur http://localhost:${port}`);
+})
